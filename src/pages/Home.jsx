d564e4f0c956b2fa8534403d761db6049f9e83c4
Yann Treuiller-Schlachter <yann.treuiller.schlachter@gmail.com>
2025-03-18 20:12:31 +0100

    Corrected product-card css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "/src/assets/styles/home.css";
-import "/src/assets/styles/Product_card.css";
+import "/src/assets/styles/product_card.css";
 
 import Hero from "../components/Hero";
 import Product_card from "../components/Product_card";
